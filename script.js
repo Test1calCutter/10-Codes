c9969851dcaf24c066f847d22731cc75f2bb254f
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-06-18 08:35:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var userLang = navigator.language || navigator.userLanguage;
+var langCode = userLang.substring(0, 2);
+
+function startCountdown(seconds) {
+    let counter = seconds;
+    document.getElementById('seconds').innerHTML = counter;
+
+    const interval = setInterval(() => {
+        counter--;
+        document.getElementById('seconds').innerHTML = counter;
+        console.log(counter)
+
+        if (counter < 1) {
+            clearInterval(interval);
+            console.log(counter)
+            console.log(langCode)
+            if (langCode === "de") {
+                window.location.assign('de/index.html')
+            }else if (langCode === "en"){
+                window.location.assign('en/index.html')
+            }
+        }
+    }, 1000);
+}
+
+startCountdown(3);
